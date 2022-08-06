faefb077b79154f3a47ffaffba57b8a65570a63b
Ed Baker <edwbaker@gmail.com>
2022-08-06 07:48:15 +0100

    Create sticker.R

---
+++ sticker.R
@@ -0,0 +1,13 @@
+library(hexSticker)
+imgurl <- "https://cdn.audioblast.org/wildlifesystems_flash.png"
+sticker(imgurl,
+        package="Wildlife Systems",
+        p_size=12,
+        p_color="black",
+        s_x=1,
+        s_y=.75,
+        s_width=.6,
+        filename="wildlfesystems_hex.png",
+        h_fill = "#D6D6D6",
+        h_color = "#888888"
+)
